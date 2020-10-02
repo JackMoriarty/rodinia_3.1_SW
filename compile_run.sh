b4cbd5c994eef35f4e7def246a4a0c46cc9d4fdf
Bangduo Chen <chenbangduo@qq.com>
2020-10-02 09:37:56 +0800

    update compile and run script

---
+++ compile_run.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+root_dir=$(pwd)
+# clean result
+rm -f result.txt
+
+# compile and run
+cd $root_dir/openacc
+dir=$(ls -l|awk '/^d/ {print $NF}')
+for i in $dir
+do
+    cd $i
+    make clean
+    make
+    for j in $(seq 5)
+    do
+        Time=$(make run|grep -i time|grep -o '[0-9]*\.[0-9]*')
+        result_string="$i:openACC:$Time"
+        echo $result_string >> $root_dir/result.txt
+    done
+    cd ..
+done
+
+cd $root_dir/athread
+dir=$(ls -l|awk '/^d/ {print $NF}')
+for i in $dir
+do
+    cd $i
+    make clean
+    make
+    for j in $(seq 5)
+    do
+        Time=$(make run|grep -i time|grep -o '[0-9]*\.[0-9]*')
+        result_string="$i:Athread:$Time"
+        echo $result_string >> $root_dir/result.txt
+    done
+    cd ..
+done
+
+echo 'Finish'
